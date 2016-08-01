0af367567eec20069905f844808183d286694fda
Casey Reed <casey.w.reed@gmail.com>
2016-08-01 13:52:24 -0500

    Giphy API working. Struggling with appending to DOM

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 console.log("script JS working")
 
 $(document).ready(function () {
+
+  var jqxhr = $.getJSON("http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC", function (res) {
+    console.log("The request worked!")
+    pictures = res.data
+    console.log(pictures)
+    pictures.forEach( function (picture) {
+      $("#photoOutput").append(`<img src="">`)
+      console.log(`${picture.embed_url}`)
+    })
+  })
+
   console.log("jQuery is ready")
 });
