142cb7ced30c2de3bd7ae444eca55dd5838605c1
Casey Reed <casey.w.reed@gmail.com>
2016-08-01 14:08:30 -0500

    Photos loading to DOM.

--- script.js
+++ script.js
@@ -2,13 +2,12 @@ console.log("script JS working")
 
 $(document).ready(function () {
 
-  var jqxhr = $.getJSON("http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC", function (res) {
+  var jqxhr = $.getJSON("http://api.giphy.com/v1/gifs/search?q=the+simpsons&api_key=dc6zaTOxFJmzC", function (res) {
     console.log("The request worked!")
     pictures = res.data
     console.log(pictures)
     pictures.forEach( function (picture) {
-      $("#photoOutput").append(`<img src="">`)
-      console.log(`${picture.embed_url}`)
+      $("#photoOutput").append(`<img src="${picture.images.fixed_height.url}"><h4></h4>`)
     })
   })
 
