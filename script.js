dc3c96276751187d3c9421bec4c446170831bab3
Casey Reed <casey.w.reed@gmail.com>
2016-08-01 14:49:31 -0500

    Starting to mess around with IF ELSE statements

--- script.js
+++ script.js
@@ -3,26 +3,24 @@ console.log("script JS working")
 $(document).ready(function () {
 
   var jqxhr = $.getJSON("http://api.giphy.com/v1/gifs/search?q=the+simpsons&api_key=dc6zaTOxFJmzC", function (res) {
-    console.log("The request worked!")
     pictures = res.data
     console.log(pictures)
     pictures.forEach( function (picture) {
       $("#photoOutput").append(`<img class="loadedPicture" src="${picture.images.fixed_height.url}">`)
     })
     $(".loadedPicture").toggleClass("hidden")
-    $(".loadedPicture").eq(0).toggleClass("hidden")
-    $(".loadedPicture").eq(0).toggleClass("active")
+    $(".loadedPicture").eq(0).toggleClass("hidden active")
   })
 
   $(".rightButton").click( function () {
-    console.log("next button clicked")
+    // if () {}
+
     $(".active").toggleClass("hidden")
     $(".active").next().toggleClass("hidden active")
     $(".active").first().toggleClass("active")
   })
 
   $(".leftButton").click( function () {
-    console.log("previous button clicked")
     $(".active").toggleClass("hidden")
     $(".active").prev().toggleClass("hidden active")
     $(".active").last().toggleClass("active")
@@ -30,3 +28,6 @@ $(document).ready(function () {
 
   console.log("jQuery is ready")
 });
+
+//Need to fix loop function.
+//If statement (if next().match(img))
