f839f9349eb2a1fc2ba9158489665eb946d3a57e
Casey Reed <casey.w.reed@gmail.com>
2016-08-01 14:36:11 -0500

    Working MVP with dual scrolling

--- script.js
+++ script.js
@@ -7,10 +7,25 @@ $(document).ready(function () {
     pictures = res.data
     console.log(pictures)
     pictures.forEach( function (picture) {
-      $("#photoOutput").append(`<img class="loadedPicture" src="${picture.images.fixed_height.url}"><h4></h4>`)
+      $("#photoOutput").append(`<img class="loadedPicture" src="${picture.images.fixed_height.url}">`)
     })
     $(".loadedPicture").toggleClass("hidden")
     $(".loadedPicture").eq(0).toggleClass("hidden")
+    $(".loadedPicture").eq(0).toggleClass("active")
+  })
+
+  $(".rightButton").click( function () {
+    console.log("next button clicked")
+    $(".active").toggleClass("hidden")
+    $(".active").next().toggleClass("hidden active")
+    $(".active").first().toggleClass("active")
+  })
+
+  $(".leftButton").click( function () {
+    console.log("previous button clicked")
+    $(".active").toggleClass("hidden")
+    $(".active").prev().toggleClass("hidden active")
+    $(".active").last().toggleClass("active")
   })
 
   console.log("jQuery is ready")
